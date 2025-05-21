6de482a0d20b89f43d5dbce9a3fa6774fab7107b
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2025-05-21 15:38:10 +0700

    Initial commit

---
+++ codingCamp/src/main/java/com/example/codingCamp/CodingCampApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingCamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingCampApplication.class, args);
+	}
+
+}
---
+++ codingCamp/src/test/java/com/example/codingCamp/CodingCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
